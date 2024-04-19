c19f6c8a0ef02bdb5f055f3f480c5917fad5f862
Guilherme Viana <imluizguilara@hotmail.com>
2024-04-19 14:17:34 -0300

    feat: finish navigation

--- src/components/layouts/default.tsx
+++ src/components/layouts/default.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from 'react'
-import { StatusBar, View } from 'react-native'
+import { StatusBar } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { LoadingIndicator } from '../loading-indicator'
+import { styles } from './style'
+import './style.ts'
 
 export const DefaultLayout = ({ children }: { children: ReactNode }) => {
   const loading = false
@@ -10,7 +13,7 @@ export const DefaultLayout = ({ children }: { children: ReactNode }) => {
       {loading ? (
         <LoadingIndicator />
       ) : (
-        <View className='w-full antialiased border border-border bg-background text-primary-foreground px-8 py-12 flex-1 h-full gap-y-3 flex items-center'>
+        <SafeAreaView style={styles.safeArea}>
           <StatusBar
             barStyle={'light-content'}
             backgroundColor={'transparent'}
@@ -18,7 +21,7 @@ export const DefaultLayout = ({ children }: { children: ReactNode }) => {
           />
 
           {children}
-        </View>
+        </SafeAreaView>
       )}
     </>
   )
---
+++ src/components/layouts/style.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  safeArea: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'hsl(0, 0%, 3.9%)',
+    color: 'hsl(0, 85.7%, 97.3%)',
+    backgroundColor: 'hsl(0, 0%, 3.9%)',
+    paddingVertical: 48,
+    paddingHorizontal: 24,
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+// theme
+// --background: 0 0% 3.9%;
+// --foreground: 0 0% 98%;
+// --card: 0 0% 3.9%;
+// --card-foreground: 0 0% 98%;
+// --popover: 0 0% 3.9%;
+// --popover-foreground: 0 0% 98%;
+// --primary: 0 72.2% 50.6%;
+// --primary-foreground: 0 85.7% 97.3%;
+// --secondary: 0 0% 14.9%;
+// --secondary-foreground: 0 0% 98%;
+// --muted: 0 0% 14.9%;
+// --muted-foreground: 0 0% 63.9%;
+// --accent: 0 0% 14.9%;
+// --accent-foreground: 0 0% 98%;
+// --destructive: 0 62.8% 30.6%;
+// --destructive-foreground: 0 0% 98%;
+// --border: 0 0% 14.9%;
+// --input: 0 0% 14.9%;
+// --ring: 0 72.2% 50.6%;
